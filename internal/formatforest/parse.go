65a4d37e585c891d128859f7539cf8d9cf88a756
Nadim Kobeissi <nadim@symbolic.software>
2020-04-19 16:46:01 +0200

    Barebones format, no publish, no clean

---
+++ internal/formatforest/parse.go
@@ -0,0 +1,70 @@
+// SPDX-FileCopyrightText: © 2020-2021 Nadim Kobeissi <nadim@nadim.computer>
+// SPDX-License-Identifier: GPL-3.0-only
+
+package formatforest
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"strings"
+)
+
+type config struct {
+	WebsiteName        string
+	WebsiteUri         string
+	WebsiteDescription string
+	WebsiteIcon        string
+	WebsiteTwitter     string
+	WebsiteLang        string
+	WebsiteLangDir     string
+	AuthorName         string
+	AuthorEmail        string
+	AuthorTwitter      string
+	AuthorLinkedIn     string
+	AuthorFacebook     string
+	AuthorInstagram    string
+}
+
+func parseConfig() config {
+	configJson := config{}
+	configBytes, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		errorExit(err)
+	}
+	configText := string(configBytes)
+	err = json.Unmarshal([]byte(configText), &configJson)
+	if err != nil {
+		errorExit(err)
+	}
+	if len(configJson.WebsiteName) == 0 {
+		errorExit(errors.New("invalid WebsiteName in config.json"))
+	}
+	if len(configJson.WebsiteUri) == 0 {
+		errorExit(errors.New("invalid WebsiteUri in config.json"))
+	}
+	if len(configJson.WebsiteIcon) == 0 {
+		configJson.WebsiteIcon = "formatforest.png"
+	}
+	if len(configJson.WebsiteLang) == 0 {
+		configJson.WebsiteLang = "en"
+	}
+	if len(configJson.WebsiteLangDir) == 0 {
+		configJson.WebsiteLangDir = "ltr"
+	}
+	if len(configJson.AuthorName) == 0 {
+		errorExit(errors.New("invalid AuthorName in config.json"))
+	}
+	return configJson
+}
+
+func parsePost(postMd string) (postConfig, string) {
+	var postconfig postConfig
+	postConfigText := strings.Join(strings.Split(postMd, "\n")[0:5], "\n")
+	err := json.Unmarshal([]byte(postConfigText), &postconfig)
+	if err != nil {
+		errorExit(err)
+	}
+	postMdContent := strings.Join(strings.Split(postMd, "\n")[5:], "\n")
+	return postconfig, postMdContent
+}
